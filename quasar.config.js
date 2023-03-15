f7a9b4d1830b353f438151ff348da2b63484d9fd
Nurbek Ismoilov <nuronbeck@gmail.com>
2023-03-16 02:21:46 +0500

    Layout

--- quasar.config.js
+++ quasar.config.js
@@ -102,6 +102,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
+      cssAddon: true,
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
